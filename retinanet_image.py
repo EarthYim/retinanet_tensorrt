379bd6cff5dd0818b38b4a12369a1b87c1ce8ec9
earthyim <kasipat.w@hotmail.com>
2020-03-17 22:55:10 +0700

    =debug retinanet_lib

--- retinanet_image.py
+++ retinanet_image.py
@@ -3,7 +3,31 @@ import numpy as np
 import cv2
 from PIL import Image
 
+def compute_resize_scale(image_shape, min_side=800, max_side=1333):
+    """ Compute an image scale such that the image size is constrained to min_side and max_side.
 
+    Args
+        min_side: The image's min side will be equal to min_side after resizing.
+        max_side: If after resizing the image's max side is above max_side, resize until the max side is equal to max_side.
+
+    Returns
+        A resizing scale.
+    """
+    (rows, cols, _) = image_shape
+
+    smallest_side = min(rows, cols)
+
+    # rescale the image so the smallest side is min_side
+    scale = min_side / smallest_side
+
+    # check if the largest side is now greater than max_side, which can happen
+    # when images have a large aspect ratio
+    largest_side = max(rows, cols)
+    if largest_side * scale > max_side:
+        scale = max_side / largest_side
+
+    return scale
+    
 def read_image_bgr(path):
     """ Read an image in BGR format.
 
